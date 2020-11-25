3346f3162258d3ca3e5d5a2ee203c3157a0f58e8
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 15:12:55 -0600

    IVehicle

---
+++ InterfaceExercise/IVehicle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public interface IVehicle
+    {
+        public int NumberOfDoors { get; set; }
+        public string DriveType { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}

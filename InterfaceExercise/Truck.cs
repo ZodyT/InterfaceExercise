a40271d53e5a4e9f437fe4d650ae7ae1ba24cb15
ZodyT <Zoraida1988@yahoo.com>
2020-11-25 15:15:59 -0600

    Truck

---
+++ InterfaceExercise/Truck.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceExercise
+{
+    public class Truck : IVehicle, ICompany
+    {
+        public int BedSize { get; set; } = 14;
+        public string EngineType { get; set; } = "Diesel";
+        int IVehicle.NumberOfDoors { get; set; } = 2;
+        string IVehicle.DriveType { get; set; } = "Manual";
+        string IVehicle.Color { get; set; } = "Red";
+        string IVehicle.Model { get; set; } = "Silverado";
+        int IVehicle.Year { get; set; } = 1995;
+        string ICompany.Name { get; set; } = "Chevrolet";
+        double ICompany.EngineSize { get; set; } = 7.5;
+    }
+}
